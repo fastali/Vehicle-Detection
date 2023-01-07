75ec14fd4170fa9e6be04c0ef1c8478fcfe6cfec
fastali <fastali@mail.ru>
2023-01-08 01:54:30 +0300

    Update point.py

--- point.py
+++ point.py
@@ -1,11 +1,11 @@
 
 class Point:
-  def __init__(self,x=0,y=0):
-    self.x=x
-    self.y=y
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
     
-  def distance(self,pointB):
-    return (((self.x-pointB.x)**2)+((self.y-pointB.y)**2))**0.5
+    def distance(self,pointB):
+        return (((self.x-pointB.x)**2)+((self.y-pointB.y)**2))**0.5
   
-  def fromPoint(self,relpointB):
-    return Point(self.x+relpointB.x,self.y+relpointB.y)
+    def fromPoint(self,relpointB):
+        return Point(self.x+relpointB.x,self.y+relpointB.y)
