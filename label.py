74cf2c643794e51bfdff07f2ec337ac6bbd22517
fastali <fastali@mail.ru>
2023-01-08 01:56:50 +0300

    Update label.py

--- label.py
+++ label.py
@@ -1,8 +1,10 @@
 from point import Point
 
 class Label:
-  def __init__(self, pointA, relpointB):
-    self.startPoint=pointA
-    self.endPoint=self.startPoint.fromPoint(relpointB)
+    def __init__(self, pointA, relpointB):
+        self.startPoint=pointA
+        self.endPoint=self.startPoint.newFromPoint(relpointB)
+        self.center=self.startPoint.newFromCoords(relpointB.x/2,relpointB.y/2)
+    
   
     
