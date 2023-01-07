729dfaebd0fd70f481c402640dcc8d3b5626b87f
fastali <fastali@mail.ru>
2023-01-08 01:52:35 +0300

    Update point.py

--- point.py
+++ point.py
@@ -6,3 +6,6 @@ class Point:
     
   def distance(self,pointB):
     return (((self.x-pointB.x)**2)+((self.y-pointB.y)**2))**0.5
+  
+  def fromPoint(self,relpointB):
+    return Point(self.x+relpointB.x,self.y+relpointB.y)
