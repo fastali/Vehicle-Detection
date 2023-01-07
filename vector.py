585683fff8d8646c835e5910d58e958354b2102f
fastali <fastali@mail.ru>
2023-01-08 01:48:50 +0300

    Create vector.py

---
+++ vector.py
@@ -0,0 +1,8 @@
+class Vector:
+  
+  def __init__(self,pointA,pointB):
+    self.startpoint=pointA
+    self.endpoint=pointB
+  
+  def lenght(self):
+    return self.startpoint.distance(self.endpoint)
