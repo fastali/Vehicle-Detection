0c2923e4a1ed4b59ded15b8d0f03ca242bdc82a6
fastali <fastali@mail.ru>
2023-01-08 01:54:55 +0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,8 +1,8 @@
 class Vector:
   
-  def __init__(self,pointA,pointB):
-    self.startpoint=pointA
-    self.endpoint=pointB
+    def __init__(self,pointA,pointB):
+        self.startpoint=pointA
+        self.endpoint=pointB
   
-  def lenght(self):
-    return self.startpoint.distance(self.endpoint)
+    def lenght(self):
+        return self.startpoint.distance(self.endpoint)
