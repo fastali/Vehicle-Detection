9c5a21741f352e1d7bc04654d05d4bccbde66cf4
fastali <fastali@mail.ru>
2023-01-10 08:47:55 +0300

    Update vector.py

--- vector.py
+++ vector.py
@@ -1,3 +1,5 @@
+from point import Point
+
 class Vector:
   
     def __init__(self,pointA,pointB):
@@ -6,3 +8,16 @@ class Vector:
   
     def lenght(self):
         return self.startpoint.distance(self.endpoint)
+    
+    def is_counter_clockwise(pointA,PointB,PointC):
+        return (PointC.y-PointA.y) * (PointB.x-PointA.x) > (PointB.y-PointA.y) * (PointC.x-PointA.x)
+    
+    def isLineCrossed(self,VectorB):
+        Condition2 = is_counter_clockwise(self.startpoint,VectorB.startpoint,VectorB.endpoint) != is_counter_clockwise(self.endpoint,VectorB.startpoint,VectorB.endpoint)
+        Condition2 = is_counter_clockwise(self.startpoint,self.endpoint,VectorB.startpoint) != is_counter_clockwise(self.startpoint,self.endpoint,VectorB.endpoint)
+        return Condition1 and Condition2
+    
+    def isCrossDirectionInward(self,VectorB):
+        return is_counter_clockwise(self.startpoint,self.endpoint,VectorB.startpoint)
+        
+      
